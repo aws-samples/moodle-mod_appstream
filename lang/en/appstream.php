c679abd39e61b9bd3bcd5bda0a1a56840f772e66
brianjbeach <social@brianbeach.com>
2018-02-05 11:34:22 -0500

    Updated header in all PHP files
    Updated header in all PHP files. Comments only. No changs to
    code/functionality.

--- lang/en/appstream.php
+++ lang/en/appstream.php
@@ -1,25 +1,27 @@
 <?php
+//Copyright 2018 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+//
 // This file is part of Moodle - http://moodle.org/
 //
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
+// Moodle is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 3
+// of the License.
+// 
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
  
 /**
  * English strings for newmodule
  *
  * @package    mod_appstream
- * @copyright  2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  2018 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 
  */
 
 defined('MOODLE_INTERNAL') || die();
